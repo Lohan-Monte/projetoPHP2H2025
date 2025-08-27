062d724d3261629d5add2e434b6b2e02f5e1b21f
Lohan-Monte <lohanmonte02@hotmail.com>
2025-08-26 22:22:11 -0300

    trabalhando com funções

--- calculadora.php
+++ calculadora.php
@@ -1,17 +1,39 @@
 <?php 
+$opcao = 2;
 
-function somaValor($num1, $num2){
-return ($num1 + $num2);
+$num1 = 20; $num2 = 13; 
+
+$op = "Subtração";
+
+if($opcao == 1){
+$respostaSoma = somaValor($num1, $num2);
+}elseif($opcao == 2){
+$repostaSub = subtraiValor($num1, $num2);
+}elseif($opcao == 3){
+$respostaMulti = multiplicaValor($num1, $num2);
+}elseif($opcao==4){
+$respostaDivisao = divideValor($num1, $num2);
+}else{
+	echo("Opção Inválida");
 }
 
-$valor1 = 20; $valor2 = 13; 
 
-function subtraiValor(){}
+function somaValor($num1, $num2){
+		return ($num1 + $num2);
+}
+function subtraiValor($num1, $num2){
+		return($num1 - $num2);
+}
+function multiplicaValor($num1, $num2){
+	return($num1 * $num2);	
+}
+function divideValor($num1, $num2){
+	return($num1 / $num2);	
+}
 
-function MultiplicaValor(){}
 
-function divideValor(){}
-
-
+echo("Os números escolhidos foram $num1 e $num2 <br/>");
+echo("A operação escolhida foi $op. <br/>");
+echo("O resultado foi $respostaSub. <br/>");
 
  ?>
