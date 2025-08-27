64cb8e46c812b40b0e37a85e740951fd183315b8
Lohan-Monte <lohanmonte02@hotmail.com>
2025-08-26 21:11:20 -0300

    Calculadora utilizando funções

---
+++ calculadora.php
@@ -0,0 +1,17 @@
+<?php 
+
+function somaValor($num1, $num2){
+return ($num1 + $num2);
+}
+
+$valor1 = 20; $valor2 = 13; 
+
+function subtraiValor(){}
+
+function MultiplicaValor(){}
+
+function divideValor(){}
+
+
+
+ ?>
--- funcoes.php
+++ funcoes.php
@@ -8,14 +8,20 @@ function titulo(){
 
 
 function somaValor($num1, $num2){
-	$resp = $num1 + $num2;
-	echo("A soma de $num1 e $num2 é $resp.");
+	return ($num1 + $num2);
 }
 
-$num1 = 10; $num2 = 20;
+$valor1 = 10; $valor2 = 20;
 
 titulo();
-somaValor($num1, $num2);
+
+$resposta = somaValor($valor1, $valor2);
+
+echo("A soma de num1 e num2 é $resposta <br/> ");
+
+$somando = somaValor(30,40) + somaValor(50,60);
+
+echo("<br/> $somando");
 
 
  ?>
