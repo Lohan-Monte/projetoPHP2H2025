6aa47c09feb8bb418173b2fae05c1cc2e7f45e54
Lohan-Monte <lohanmonte02@hotmail.com>
2025-10-14 20:22:25 -0300

    Add files via upload

---
+++ forms.php
@@ -0,0 +1,28 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Formularops</title>
+</head>
+<body>
+
+<h1>Formulário de cadastro</h1>
+
+<form action="cadastrar.php" method="post">
+	<label for="nome">Nome:</label> <input type="text" name="nome" id="nome">
+	<br><br>
+	<label for="email">Email:</label> <input type="email" name="email" id="email">
+	<br><br>
+	<input type="submit" name="enviar">
+	<br><br>
+	<button type="submit">Cadastrar</button>
+</form>
+	
+	
+</body>
+</html>
+
+
+
